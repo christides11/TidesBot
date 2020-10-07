7a9794aa9af839dda2a0244fba0aa34afaecbe09
christides11 <29593332+christides11@users.noreply.github.com>
2020-10-07 15:32:51 -0700

    Added module for adding roles when a user joins the server.

---
+++ Modules/AutoRolesModule.cs
@@ -0,0 +1,62 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TidesBotDotNet.Services;
+
+namespace TidesBotDotNet.Modules
+{
+    [Group("autoroles")]
+    public class AutoRolesModule : ModuleBase<SocketCommandContext>
+    {
+        public static AutoRolesService autoRolesService;
+        private DiscordSocketClient client;
+
+        public AutoRolesModule(DiscordSocketClient client)
+        {
+            if(autoRolesService == null)
+            {
+                autoRolesService = new AutoRolesService(client);
+            }
+            this.client = client;
+        }
+
+        [Command("addrole")]
+        [Alias("ar")]
+        [Summary("Adds a role to be auto assigned to users.")]
+        [RequireUserPermission(GuildPermission.Administrator, Group = "Permission")]
+        [RequireOwner(Group = "Permission")]
+        public async Task AddRole(string role)
+        {
+            SocketRole wantedRole = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToLower() == role.ToLower());
+            if(wantedRole == null)
+            {
+                await Context.Channel.SendMessageAsync($"Role {role} not found.");
+                return;
+            }
+            await Context.Channel.SendMessageAsync(autoRolesService.AddRole(Context.Guild, wantedRole));
+            autoRolesService.Save();
+        }
+
+        [Command("removerole")]
+        [Alias("rr")]
+        [Summary("Removes a role to be auto assigned to users.")]
+        [RequireUserPermission(GuildPermission.Administrator, Group = "Permission")]
+        [RequireOwner(Group = "Permission")]
+        public async Task RemoveRole(string role)
+        {
+            SocketRole wantedRole = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToLower() == role.ToLower());
+            if (wantedRole == null)
+            {
+                await Context.Channel.SendMessageAsync($"Role {role} not found.");
+                return;
+            }
+            await Context.Channel.SendMessageAsync(autoRolesService.RemoveRole(Context.Guild, wantedRole));
+            autoRolesService.Save();
+        }
+    }
+}
---
+++ Services/AutoRolesService.cs
@@ -0,0 +1,115 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidesBotDotNet.Services
+{
+    public class AutoRolesService
+    {
+        public class AutoRolesGuildDefinition
+        {
+            public ulong guildID;
+            public List<string> roles = new List<string>();
+
+            public AutoRolesGuildDefinition(ulong guildID)
+            {
+                this.guildID = guildID;
+                roles = new List<string>();
+            }
+        }
+
+        private readonly string monitoredUsersFilename = "autoroles.json";
+
+        private DiscordSocketClient client;
+
+        public List<AutoRolesGuildDefinition> autoRoles = new List<AutoRolesGuildDefinition>();
+
+        public AutoRolesService(DiscordSocketClient client)
+        {
+            this.client = client;
+            autoRoles = SaveLoadService.Load<List<AutoRolesGuildDefinition>>(monitoredUsersFilename);
+            if(autoRoles == null)
+            {
+                autoRoles = new List<AutoRolesGuildDefinition>();
+                SaveLoadService.Save(monitoredUsersFilename, autoRoles);
+            }
+
+            client.UserJoined += OnUserJoinedGuild;
+        }
+
+        private async Task OnUserJoinedGuild(SocketGuildUser user)
+        {
+            AutoRolesGuildDefinition arg = autoRoles.FirstOrDefault(x => x.guildID == user.Guild.Id);
+            // Guild doesn't auto assign any roles.
+            if(arg == null)
+            {
+                return;
+            }
+            // Give the user every role they should have.
+            foreach(string role in arg.roles)
+            {
+                await GiveRole(user, role);
+            }
+        }
+
+        public async Task GiveRole(SocketGuildUser user, string role)
+        {
+            SocketRole realRole = user.Guild.Roles.FirstOrDefault(x => x.Name.ToLower() == role.ToLower());
+
+            // If the user doesn't have the role, give it to them.
+            if (user.Guild.GetUser(user.Id).Roles.FirstOrDefault(x => x.Name == realRole.Name) == null)
+            {
+                try
+                {
+                    await user.Guild.GetUser(user.Id).AddRolesAsync(new List<SocketRole>() { realRole });
+                    Console.WriteLine($"Gave user {user.Nickname} role {realRole}.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not give user role! " + e.Message);
+                }
+            }
+        }
+
+        public string AddRole(SocketGuild guild, SocketRole role)
+        {
+            AutoRolesGuildDefinition arg = autoRoles.FirstOrDefault(x => x.guildID == guild.Id);
+            if(arg == null)
+            {
+                arg = new AutoRolesGuildDefinition(guild.Id);
+                autoRoles.Add(arg);
+            }
+            if(!arg.roles.Contains(role.Name.ToLower()))
+            {
+                arg.roles.Add(role.Name.ToLower());
+                return $"Role {role.Name} added.";
+            }
+
+            return $"{role.Name} is already in the list.";
+        }
+
+        public string RemoveRole(SocketGuild guild, SocketRole role)
+        {
+            AutoRolesGuildDefinition arg = autoRoles.FirstOrDefault(x => x.guildID == guild.Id);
+            if (arg == null)
+            {
+                return "No roles being assigned in this guild.";
+            }
+            if (arg.roles.Contains(role.Name.ToLower()))
+            {
+                arg.roles.Remove(role.Name.ToLower());
+                return $"Role {role.Name} removed.";
+            }
+            return $"Role {role.Name} is not in the list.";
+        }
+
+        public void Save()
+        {
+            SaveLoadService.Save(monitoredUsersFilename, autoRoles);
+        }
+    }
+}
